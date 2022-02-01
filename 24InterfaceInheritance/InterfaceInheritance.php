25e3c22d0f43b6b0f0922cad75c27541d7ca6cac
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-02-01 20:59:40 +0700

    Update Materi: Interface Inheritance

--- 24InterfaceInheritance/InterfaceInheritance.php
+++ 24InterfaceInheritance/InterfaceInheritance.php
@@ -3,7 +3,7 @@
 // > Interface Inheritance
 // 1. Sebelumnya kita sudah tahu kalo di PHP, child class hanya bisa punya 1 class parent.
 // 2. Namun berbeda dengan interface, sebuah child class bisa implement lebih dari 1 interface
-// 3. Bahkan interface pun bisa implement interface lain, bisa lebih dari 1. Namun jika interface ingin mewarisi interface lain, kita menggunakan kata kunci extends, bukan implements
+// 3. Bahkan interface pun bisa implement interface lain, bisa lebih dari 1. Namun jika interface ingin mewarisi interface lain, kita menggunakan kata kunci "extends", bukan implements
 
 // > Implementasi Interface Inheritance
 namespace Data;
