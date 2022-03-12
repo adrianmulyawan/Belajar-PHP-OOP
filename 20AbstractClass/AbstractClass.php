16c895bce873228b111ed24149f8f8cf75ca24e6
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-03-12 18:41:49 +0700

    update materi abstract class

--- 20AbstractClass/AbstractClass.php
+++ 20AbstractClass/AbstractClass.php
@@ -2,7 +2,7 @@
 
 // > Abstract Class
 // 1. Saat kita membuat sebuah class, kita bisa menjadikan sebuah class sebagai abstract class
-// 2. Abstract class artinya, class tersebut bisa dibuat sebagai object secara langsung, hanya bisa diturunkan
+// 2. Abstract class artinya, class tersebut tidak bisa dibuat sebagai object secara langsung, hanya bisa diturunkan
 // 3. Untuk membuat sebuah class menjadi abstract, kita bisa menggunakan kata kunci "abstract" sebelum kata kunci class
 // 4. Sehingga Abstract Class bisa kita gunakan sebagai kontrak child class
 
