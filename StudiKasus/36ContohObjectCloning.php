047dd93759e73956aef225976f15e0d02ac96d23
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-03-17 12:56:46 +0700

    update materi object cloning

--- StudiKasus/36ContohObjectCloning.php
+++ StudiKasus/36ContohObjectCloning.php
@@ -41,7 +41,7 @@ var_dump($student1);
 $student2 = clone $student1;
 var_dump($student2);
 // Hasil
-//   object(Data\Students\Student)#2 (3) {
+// object(Data\Students\Student)#2 (3) {
 //     ["nim"]=>
 //     string(8) "15523115"
 //     ["name"]=>
@@ -50,4 +50,19 @@ var_dump($student2);
 //     int(100)
 //     ["sample":"Data\Students\Student":private]=>
 //     uninitialized(string)
-//   }
+// }
+
+# Kita coba set Samplenya
+$student2->setSample('Wasyu');
+var_dump($student2);
+# Hasil
+// object(Data\Students\Student)#2 (4) {          
+//     ["nim"]=>                                    
+//     string(8) "15523115"                         
+//     ["name"]=>                                   
+//     string(15) "Adrian Mulyawan"                 
+//     ["value"]=>                                  
+//     int(100)                                     
+//     ["sample":"Data\Students\Student":private]=> 
+//     string(5) "Wasyu"                            
+// }                                              
